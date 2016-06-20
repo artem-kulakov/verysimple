0f3499cc14a5101ca8dc3498293e8d30388d7f70
Artem Kulakov <artem.kulakov@gmail.com>
2016-06-21 00:50:19 +0300

    Refactor the navbar logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_active_nav_item
+
+  def set_active_nav_item
+  	instance_variable_set("@#{controller_name}_active", 'active')
+  end
 end
