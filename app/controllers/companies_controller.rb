ce057d24845a1a36967ae0f69ecb4d05319aaecf
Artem Kulakov <artem.kulakov@gmail.com>
2016-06-27 21:46:48 +0300

    Adjust company#edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -27,6 +27,7 @@ class CompaniesController < ApplicationController
 
   # GET /companies/1/edit
   def edit
+    @path = 'companies_path'
   end
 
   # POST /companies
@@ -54,7 +55,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to companies_path, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
