bc6c4036ce960a95f5586707300d08cadc250835
Artem Kulakov <artem.kulakov@gmail.com>
2016-07-25 22:26:19 +0300

    Add periods to navbar and seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ companies.each do |company|
   Company.create!(name: company)
 end
 
-
 # Indicators
 indicators = ['Revenues',
              'Operating income',
@@ -17,15 +16,24 @@ end
 
 # Records
 Company.first(3).each do |company|
-	Record.create!(company_id: company.id)
+  Record.create!(company_id: company.id)
 end
 
 # Values
 amounts = [1000, 100, 1500,
-					 2000, 200, 3000,
-					 3000, 300, 4500]
+           2000, 200, 3000,
+           3000, 300, 4500]
 Record.all.each do |record|
-	Indicator.all.each do |indicator|
-		Value.create!(record_id: record.id, indicator_id: indicator.id, amount: amounts.shift)
-	end
+  Indicator.all.each do |indicator|
+    Value.create!(record_id: record.id, indicator_id: indicator.id, amount: amounts.shift)
+  end
+end
+
+# Periods
+endings = ['2016-06-30',
+           '2016-03-31',
+           '2015-12-31',
+           '2014-12-31']
+endings.each do |ending|
+  Period.create!(ending: ending)
 end
