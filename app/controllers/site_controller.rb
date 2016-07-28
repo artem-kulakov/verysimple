ea4d9f323d1df2ce1b5f199ca08174858962d66e
Artem Kulakov <artem.kulakov@gmail.com>
2016-07-29 00:38:17 +0300

    Add selection of gaaps to the root page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,8 +3,11 @@ class SiteController < ApplicationController
     @periods = Period.order(:ending)
     @period = Period.find(params[:period] || 1)
 
+    @gaaps = Gaap.order(:name)
+    @gaap = Gaap.find(params[:gaap] || 1)
+
   	@indicators = Indicator.order(:order)
-  	@records = Record.where(period_id: @period)
+  	@records = Record.where(period_id: @period, gaap_id: @gaap)
   	@values = Value.all
   end
 end
