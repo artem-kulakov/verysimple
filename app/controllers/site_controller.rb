c2c68da6f7884177c05049cfe2ad3c77f9557402
Artem Kulakov <artem.kulakov@gmail.com>
2016-07-29 00:12:32 +0300

    Add selection of period in root page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,10 @@
 class SiteController < ApplicationController
   def index
+    @periods = Period.order(:ending)
+    @period = Period.find(params[:period] || 1)
+
   	@indicators = Indicator.order(:order)
-  	@records = Record.all
+  	@records = Record.where(period_id: @period)
   	@values = Value.all
   end
 end
