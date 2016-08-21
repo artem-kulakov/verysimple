706c540473be2200210fa7dbc9be4f56f5666fda
Artem Kulakov <artem.kulakov@gmail.com>
2016-08-21 17:38:09 +0300

    Generate model Currency

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821143108) do
+ActiveRecord::Schema.define(version: 20160821143739) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "currencies", force: :cascade do |t|
+    t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
