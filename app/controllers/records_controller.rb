73e22663fa0ea3e653141589353a6b05a42f97bb
Artem Kulakov <artem.kulakov@gmail.com>
2016-09-27 19:08:38 +0300

    Refactor again; add flash[:points]

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -63,14 +63,17 @@ class RecordsController < ApplicationController
   def create
     # Increase user's reputation
     values = record_params[:values_attributes].count { |index, params| not params[:amount].empty? }
-    reputation = current_user.reputation + values * 10
+    current_user_reputation = current_user.reputation + values * 10
     @record = Record.new(record_params.merge(user_id: current_user.id))
+
+    points = current_user_reputation - current_user.reputation
+    flash[:points] = sprintf("%+d", points) + ' points for new record'
 
     respond_to do |format|
       if @record.save
         format.html { redirect_to root_path(period: record_params[:period_id], gaap: record_params[:gaap_id]), notice: 'Record was successfully created.' }
         format.json { render :show, status: :created, location: @record }
-        current_user.update(reputation: reputation)
+        current_user.update(reputation: current_user_reputation)
       else
         format.html { render :new }
         format.json { render json: @record.errors, status: :unprocessable_entity }
@@ -82,65 +85,70 @@ class RecordsController < ApplicationController
   # PATCH/PUT /records/1.json
   def update
     respond_to do |format|
-      # Change user's reputation
-      old_values = @record.values.where.not(amount: nil).count
-      new_values = record_params[:values_attributes].count { |index, params| not params[:amount].empty? }
-      values = new_values - old_values
-      reputation = current_user.reputation + values * 10
-
-      old_values = []
-      @record.values.each do |value|
-        old_values << value.amount
-      end
-
-      new_values = []
-      record_params[:values_attributes].each do |k, value|
-        new_values << value[:amount]
-      end
-
-      actions = []
-      old_values.each_with_index do |old_value, index|
-        if not old_value.nil? and new_values[index].empty?
-          actions << 'deleted'
-        elsif old_value.nil? and not new_values[index].empty?
-          actions << 'new'
-        elsif old_value.nil? and new_values[index].empty?
-          actions << 'nothing'
-        else
-          actions << new_values[index].to_f / old_value - 1
-        end
-      end
-
       old_user = @record.user
-      old_user_reputation = old_user.reputation
-      new_user_reputation = current_user.reputation
-      actions.each do |action|
-        if action == 'new'
-          new_user_reputation += 10
-        elsif action == 'deleted'
-          new_user_reputation -= 5
-        elsif action == 'nothing'
-          #
-        else
-          if action.abs <= 0.05
-            old_user_reputation -= action.abs * 100
-            new_user_reputation += action.abs * 100
+      
+      if current_user.id == old_user.id
+        old_values = @record.values.where.not(amount: nil).count
+        new_values = record_params[:values_attributes].count { |index, params| not params[:amount].empty? }
+        current_user_reputation = current_user.reputation + (new_values - old_values) * 10
+      else
+        old_values = []
+        @record.values.each do |value|
+          old_values << value.amount
+        end
+
+        new_values = []
+        record_params[:values_attributes].each do |k, value|
+          new_values << value[:amount]
+        end
+
+        actions = []
+        old_values.each_with_index do |old_value, index|
+          if not old_value.nil? and new_values[index].empty?
+            actions << 'deleted'
+          elsif old_value.nil? and not new_values[index].empty?
+            actions << 'new'
+          elsif old_value.nil? and new_values[index].empty?
+            actions << 'nothing'
           else
-            old_user_reputation -= 5
-            new_user_reputation += 5
+            actions << new_values[index].to_f / old_value - 1
+          end
+        end
+
+        old_user_reputation = old_user.reputation
+        current_user_reputation = current_user.reputation
+        actions.each do |action|
+          if action == 'new'
+            current_user_reputation += 10
+          elsif action == 'deleted'
+            current_user_reputation -= 5
+          elsif action == 'nothing'
+            #
+          else
+            if action.abs <= 0.05
+              old_user_reputation -= action.abs * 100
+              current_user_reputation += action.abs * 100
+            else
+              old_user_reputation -= 5
+              current_user_reputation += 5
+            end
           end
         end
       end
 
+      points = current_user_reputation - current_user.reputation
+      flash[:points] = sprintf("%+d", points) + ' points for editing record'
+
+      # Update record
       if @record.update(record_params.merge(user_id: current_user.id))
         format.html { redirect_to root_path, notice: 'Record was successfully updated.' }
         format.json { render :show, status: :ok, location: @record }
 
         if current_user.id == old_user.id
-          current_user.update(reputation: reputation)
+          current_user.update(reputation: current_user_reputation)
         else
           old_user.update(reputation: old_user_reputation)
-          current_user.update(reputation: new_user_reputation)
+          current_user.update(reputation: current_user_reputation)
         end
       else
         format.html { render :edit }
