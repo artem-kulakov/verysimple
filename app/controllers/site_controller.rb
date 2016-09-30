d724bdf2e03e27109b4b4194e15fd75a09f66bdc
Artem Kulakov <artem.kulakov@gmail.com>
2016-09-30 18:44:05 +0300

    Add currency selector to root page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,6 +6,9 @@ class SiteController < ApplicationController
     @gaaps = Gaap.order(:name)
     @gaap = Gaap.find(params[:gaap] || 1)
 
+    @currencies = Currency.order(:code)
+    @currency = Currency.find(params[:currency] || 152)
+
   	@indicators = Indicator.order(:order)
   	@records = Record.where(period_id: @period, gaap_id: @gaap)
   	@values = Value.all
--- app/models/value.rb
+++ app/models/value.rb
@@ -3,13 +3,13 @@ class Value < ActiveRecord::Base
   belongs_to :indicator
   belongs_to :unit
 
-  def self.amount(record_id, indicator_id)
+  def self.amount(record_id, indicator_id, target_currency_id)
   	value = where(record_id: record_id, indicator_id: indicator_id).first
   	unless value.amount.nil?
       if value.unit.type.name == 'currency'
-        currency_id = Currency.where(code: value.unit.name).pluck(:id).first
-        original_to_usd = Rate.where(period_id: value.record.period_id, currency_id: currency_id).pluck(:rate).first
-        usd_to_target = Rate.where(period_id: value.record.period_id, currency_id: 48).pluck(:rate).first
+        original_currency_id = Currency.where(code: value.unit.name).pluck(:id).first
+        original_to_usd = Rate.where(period_id: value.record.period_id, currency_id: original_currency_id).pluck(:rate).first
+        usd_to_target = Rate.where(period_id: value.record.period_id, currency_id: target_currency_id).pluck(:rate).first
         value.amount / original_to_usd * usd_to_target
       else
   		  value.amount * value.unit.multiplier
