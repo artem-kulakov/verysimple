959bf6174f83fdf31f7dd0a9b9953a3b6305383f
Artem Kulakov <artem.kulakov@gmail.com>
2016-10-07 08:24:16 +0300

    Add forum to navbar; set forum authentication

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   # GET /topics
