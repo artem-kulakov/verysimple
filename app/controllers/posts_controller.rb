f56548312e1b76aaa4c9faaf54d85e11ddfa6560
Artem Kulakov <artem.kulakov@gmail.com>
2016-10-17 18:51:42 +0300

    Style and tune posts#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @topic = Topic.find(params[:topic])
   end
 
   # GET /posts/1/edit
@@ -25,11 +26,11 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post.topic, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -70,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:user_id, :topic_id, :body)
+      params.require(:post).permit(:topic_id, :body)
     end
 end
